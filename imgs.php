70063dc3b3d3c1a339ec953dd96de9aa3a8fc957
910JQK <v910JQK@163.com>
2012-12-02 01:28:32 +0800

    1202

--- imgs.php
+++ imgs.php
@@ -1,14 +1,21 @@
-<div class="a1">
-<img src="3.png" name="i1" onmouseOver="mouseOver_i1()" onmouseOut="mouseOut_i1()" onmouseDown="mouseDown_i1()" onmouseUp="mouseUp_i1()" />
+<div class="block a b1">
+
 </div>
-<div class="a2">
-<img src="3.png" name="i2" onmouseOver="mouseOver_i2()" onmouseOut="mouseOut_i2()" onmouseDown="mouseDown_i2()" onmouseUp="mouseUp_i2()" />
+<div class="block a b2">
+
 </div>
-<div class="a3">
-<img src="3.png" name="i3" onmouseOver="mouseOver_i3()" onmouseOut="mouseOut_i3()" onmouseDown="mouseDown_i3()" onmouseUp="mouseUp_i3()" />
+<div class="block a b3">
+
 </div>
-<div class="a4">
-<img src="3.png" name="i4" onmouseOver="mouseOver_i4()" onmouseOut="mouseOut_i4()" onmouseDown="mouseDown_i4()" onmouseUp="mouseUp_i4()" />
+<div class="block a b4">
+
 </div>
-<div class="b_b" id="pdate"></div>
-<div class="b_b" id="psec"></div>
+
+
+<div class="block b_b" id="pdate"></div>
+
+<div class="block b_b" id="psec"></div>
+
+<div class="block b_b" id="uname"><?php system('uname -snrmo'); ?></div>
+
+<div class="block b_b" id="df"><pre><?php system('df -lh'); ?></pre></div>
