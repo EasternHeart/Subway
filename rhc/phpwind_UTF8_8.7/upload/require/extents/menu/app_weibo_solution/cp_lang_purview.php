9c9c974b55528b366993950aa447eaf4a9e5d1ed
910JQK <v910JQK@163.com>
2012-12-07 19:27:08 +0800

    1

--- rhc/phpwind_UTF8_8.7/upload/require/extents/menu/app_weibo_solution/cp_lang_purview.php
+++
@@ -1,6 +0,0 @@
-<?php
-!defined('P_W') && exit('Forbidden');
-
-unset($purview['sinaweibo']); //for compatible
-$purview['platformweiboapp'] = array('帐号通',"$admin_file?adminjob=app&admintype=platformweiboapp");
-?>
